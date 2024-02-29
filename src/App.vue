8c4b1ebc92126cea7fa26a327f9a3aa4a4659a0b
emanuele-pogliari <manu.pogliari@gmail.com>
2024-02-29 15:36:02 +0100

    first commit - basic structure

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import AppNav from './components/AppNav.vue';
+
+export default {
+  components: {
+    AppNav,
+  }
+}
+
+</script>
+
+<template>
+  <AppNav>ciao</AppNav>
+</template>
+
+<style lang="scss">
+@use './styles/general' as *;
+</style>
---
+++ src/components/AppNav.vue
@@ -0,0 +1,10 @@
+<script>
+export default {
+    name: 'AppNav',
+}
+</script>
+
+<template>
+    ciao
+</template>
+<style></style>
