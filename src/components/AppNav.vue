c6c34c1448754f100d05a118f5a0e8c8257a2cd8
emanuele-pogliari <manu.pogliari@gmail.com>
2024-02-29 16:06:29 +0100

    navbar completed

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -5,6 +5,48 @@ export default {
 </script>
 
 <template>
-    ciao
+    <header>
+        <nav class="nav-container">
+            <img class="img-logo" src="../assets/img/dc-logo.png" alt="">
+            <ul class="nav-links">
+                <li>Characters</li>
+                <li>Comics</li>
+                <li>Movies</li>
+                <li>Tv</li>
+                <li>Games</li>
+                <li>Collectibles</li>
+                <li>Videos</li>
+                <li>Fans</li>
+                <li>News</li>
+                <li>Show</li>
+            </ul>
+        </nav>
+    </header>
 </template>
-<style></style>
+
+<style lang="scss">
+header {
+    margin: auto;
+}
+
+.nav-container {
+    display: flex;
+    justify-content: space-between;
+    width: 70%;
+    margin: auto;
+    padding: 15px;
+
+    .nav-links {
+        display: flex;
+        align-items: center;
+        list-style-type: none;
+        gap: 20px;
+        text-transform: uppercase;
+    }
+
+    .img-logo {
+        width: 80px;
+    }
+
+}
+</style>
